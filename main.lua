4ad7f46ed6ed2b06713adae6e02a5986025f1650
Xkeeper <xkeeper@gmail.com>
2017-12-03 11:08:13 -0800

    Add some question code

--- main.lua
+++ main.lua
@@ -1,6 +1,27 @@
--- Stuff
+-- Decisions, a LD40 game that won't be finished in time as usual
 
+
+-- Gamestate manager
+Gamestate	= require "hump.gamestate"
+
+-- Questions for our game.
+Questions	= require "questions"
+q			= Questions.getQuestion(1)
+
+function love.load(arg)
+	-- Set the random seed so we don't get the same thing
+	-- over and over
+	love.math.setRandomSeed(love.timer.getTime())
+end
 
 function love.draw()
 	love.graphics.print("Hello world", 400, 300)
+	love.graphics.print(q.question, 300, 320)
+	love.graphics.print(q.answer1, 300, 340)
+	love.graphics.print(q.answer2, 300, 360)
+end
+
+
+function love.keypressed(key, scancode, isrepeat)
+	q			= Questions.getQuestion(1)
 end
---
+++ questions.lua
@@ -0,0 +1,30 @@
+local Questions		= {}
+
+-- 4 levels of questions
+local actualQuestions	= { {}, {}, {}, {} }
+
+-- Simple helper function to avoid having to write {question = "xxx", answer1 = "..."}
+-- over and over again
+local function addQuestion(level, question, answer1, answer2)
+	table.insert(actualQuestions[level], { question = question, answer1 = answer1, answer2 = answer2})
+end
+
+addQuestion(1, "test question 1", "yes", "no")
+addQuestion(1, "test question 2", "yes", "no")
+addQuestion(1, "test question 3", "yes", "no")
+addQuestion(1, "test question 4", "yes", "no")
+addQuestion(1, "test question 5", "yes", "no")
+
+-- Get a random question for the selected level
+function Questions.getQuestion(level)
+	local count	= #actualQuestions[level]
+	if count == 0 then
+		-- Out of questions, oops
+		return { question = "...no more questions", answer1 = "error", answer2 = "error" }
+	end
+
+	local r	= math.random(1, #actualQuestions[level])
+	return table.remove(actualQuestions[level], r)
+end
+
+return Questions
